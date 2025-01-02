104c132d695ddd9742ffd4fed9c6d7385101a797
Oluwadara Oloye <daraoloye99@gmail.com>
2025-01-02 23:21:11 +0100

    fix: ensured changes in env are registered

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 BLOCKONOMICS_BASE_API_URL = os.getenv("BLOCKONOMICS_BASE_API_URL")
 BLOCKONOMICS_API_KEY = os.getenv("BLOCKONOMICS_API_KEY")
 
@@ -19,11 +19,13 @@ async def get_btc_price(currency_code='USD'):
       return {"error": "Failed to retrieve data"}
 
 async def get_new_address():
+  print("BLOCKONOMICS_API_KEY =>", BLOCKONOMICS_API_KEY)
   url = f"{BLOCKONOMICS_BASE_API_URL}/new_address?reset=1"
   headers = {
     "Authorization": f"Bearer {BLOCKONOMICS_API_KEY}"
   }
   response = requests.post(url, json={}, headers=headers)
+  print(f"RESPONSE => {response}")
   if response.status_code == 200:
     return response.json()
   else:
